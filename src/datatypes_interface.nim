d367ba8a70c90765736465950823de77749b13df
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2018-03-26 21:43:03 +0300

    Use nimcrypto instead of keccak_tiny

--- src/datatypes_interface.nim
+++ src/datatypes_interface.nim
@@ -15,7 +15,7 @@
 
 import  ./datatypes, ./private/conversion_bytes
 
-import keccak_tiny
+import nimcrypto
 
 when defined(backend_native):
   import ./backend_native/ecdsa
@@ -43,28 +43,32 @@ proc initPublicKey*(hexString: string): PublicKey {.noInit.} =
 # ################################
 # Public key/signature interface
 
-proc recover_pubkey_from_msg*(message_hash: Hash[256], sig: Signature): PublicKey {.inline.} =
+proc recover_pubkey_from_msg*(message_hash: MDigest[256], sig: Signature): PublicKey {.inline.} =
   ecdsa_recover(message_hash, sig)
 
 proc recover_pubkey_from_msg*(message: string, sig: Signature): PublicKey {.inline.} =
-  let message_hash = keccak_256(message)
+  let message_hash = keccak256.digest(message)
   ecdsa_recover(message_hash, sig)
 
-proc verify_msg*(key: PublicKey, message_hash: Hash[256], sig: Signature): bool {.inline.} =
+proc verify_msg*(key: PublicKey, message_hash: MDigest[256], sig: Signature): bool {.inline.} =
   key == ecdsa_recover(message_hash, sig)
 
 proc verify_msg*(key: PublicKey, message: string, sig: Signature): bool {.inline.} =
-  let message_hash = keccak_256(message)
+  let message_hash = keccak256.digest(message)
   key == ecdsa_recover(message_hash, sig)
 
 # # ################################
 # # Private key interface
 
-proc sign_msg*(key: PrivateKey, message: string): Signature {.inline.} =
-  let message_hash = keccak_256(message)
+proc sign_msg*(key: PrivateKey, message: openarray[byte]): Signature {.inline.} =
+  let message_hash = keccak256.digest(message)
   ecdsa_sign(key, message_hash)
 
-proc sign_msg*(key: PrivateKey, message_hash: Hash[256]): Signature {.inline.} =
+proc sign_msg*(key: PrivateKey, message: string): Signature {.inline.} =
+  let message_hash = keccak256.digest(message)
+  ecdsa_sign(key, message_hash)
+
+proc sign_msg*(key: PrivateKey, message_hash: MDigest[256]): Signature {.inline.} =
   ecdsa_sign(key, message_hash)
 
 proc `$`*(key: PrivateKey): string {.inline.} =
